02c9f56d2288e64667ea4fd66e9641390cfc15f5
ksm <hugokang4@gmail.com>
2022-11-26 16:16:59 -0500

    initial commit

--- .idea/go_learning_basic/pointer_5/main.go
+++ .idea/go_learning_basic/pointer_5/main.go
@@ -1,15 +1,21 @@
 package main
+
 import "fmt"
 
-func main(){
+func main() {
 	var i int = 10
 
 	fmt.Println("the address of i is: ", &i)
-
 
 	// 1. ptr is a pointer variable
 	// 2. the tyupe of ptr is *int
 	// 3. the value of ptr is &i
 	var ptr *int = &i
 	fmt.Println(ptr)
+
+	var n int = 9
+	fmt.Println("address of n is:", &n)
+
+	var ptr2 *int = &n
+	fmt.Println("address of n is:", ptr2)
 }
