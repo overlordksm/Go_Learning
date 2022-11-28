cef2e55d3c040b25a03fe8bcf225fec0ed99ba9e
ksm <hugokang4@gmail.com>
2022-11-28 18:24:16 -0500

    initial commit

--- .idea/go_learning_basic/function_9/main.go
+++ .idea/go_learning_basic/function_9/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "Go_Learning/.idea/go_learning_basic/function_9/utils"
+
 func main() {
+	utils.Calculate(1.1, 2.2, '+')
+
+	utils.Calculate(1.1, 2.2, '-')
+
+	utils.Calculate(1.1, 2.2, '*')
+
+	utils.Calculate(1.1, 2.2, '/')
 
 }
